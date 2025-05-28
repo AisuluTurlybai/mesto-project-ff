96e08dfc38fde46eaca9e32b5444964031bf1e9a
AisuluTurlybai <ais.turlybai@gmail.com>
2025-05-28 09:57:27 +0500

    code review fix 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,28 @@
 // @todo: Темплейт карточки
   const cardsList = document.querySelector('.places__list');
 
-function addCards (dataCards) {
+const createCards = dataCards => {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   const cardButton = cardElement.querySelector('.card__delete-button');
   
   cardElement.querySelector('.card__image').src = dataCards.link;
+  cardElement.querySelector('.card__image').alt = dataCards.name;
   cardElement.querySelector('.card__title').textContent = dataCards.name;
 
-  cardButton.addEventListener('click', function () {
+  cardButton.addEventListener('click', () => {
      deleteCard(cardElement);
   });
 
   return cardElement;
 }
 
-function deleteCard (removeCard) {
+const deleteCard = removeCard => {
     removeCard.remove();
     return;
 }
 
-initialCards.forEach (function(item) {
- const card = addCards(item, deleteCard);
+initialCards.forEach ((item) => {
+ const card = createCards(item, deleteCard);
   cardsList.append(card);
 })
